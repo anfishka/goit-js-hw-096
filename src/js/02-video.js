0a1763674ec409d91426fd3197a16bec6cb9707a
Hanna Borodina <venera131916@gmail.com>
2023-12-18 11:18:23 +0200

    completed 3 tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Vimeo from "@vimeo/player"
+import throttle from "lodash/throttle";
 
+let iframe = document.getElementById('vimeo-player');
+let player = new Vimeo(iframe);
+
+let throttledUpdate = throttle((time) => {
+    localStorage.setItem('videoplayer-current-time', time);
+}, 1000);
+
+player.on('timeupdate', (data) => {
+    throttledUpdate(data.seconds);
+});
+
+let currentTime = localStorage.getItem('videoplayer-current-time');
+if(currentTime)
+{
+    player.setCurrentTime(parseFloat(currentTime));
+}
